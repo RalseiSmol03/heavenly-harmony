c21e502fe822513780a7960027525290062752ab
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-07 23:18:23 +0300

    ...

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -18,9 +18,6 @@ using StringTools;
 
 class AtlasFrameMaker extends FlxFramesCollection
 {
-	private var frameCollection:FlxFramesCollection;
-	private var frameArray:Array<Array<FlxFrame>> = [];
-
 	public static function construct(key:String, ?excludeFrames:Array<String>, ?noAntialiasing:Bool = false):FlxFramesCollection
 	{
 		if (Paths.fileExists('images/$key/spritemap1.json', TEXT))
@@ -32,20 +29,17 @@ class AtlasFrameMaker extends FlxFramesCollection
 			return null;
 		}
 
-		var animationData:AnimationData = Json.parse(Paths.getTextFromFile('images/$key/Animation.json'));
+		var animData:AnimationData = Json.parse(Paths.getTextFromFile('images/$key/Animation.json'));
 		var atlasData:AtlasData = Json.parse(Paths.getTextFromFile('images/$key/spritemap.json').replace("\uFEFF", ""));
 
-		var s:SpriteAnimationLibrary = new SpriteAnimationLibrary(animationData, atlasData, Paths.image('$key/spritemap').bitmap);
-		var t:SpriteMovieClip = s.createAnimation(noAntialiasing);
+		var sprAnimLib:SpriteAnimationLibrary = new SpriteAnimationLibrary(animData, atlasData, Paths.image('$key/spritemap').bitmap);
+		var sprMovieClip:SpriteMovieClip = sprAnimLib.createAnimation(noAntialiasing);
 
-		frameCollection = new FlxFramesCollection(graphic, IMAGE);
-
-		for (i in excludeFrames == null ? t.getFrameLabels() : excludeFrames)
-			frameArray.push(getFramesArray(t, i));
-
-		for (i in frameArray)
-			for (j in i)
-				frameCollection.pushFrame(j);
+		var frameCollection:FlxFramesCollection = new FlxFramesCollection(graphic, IMAGE);
+		for (i in excludeFrames == null ? sprMovieClip.getFrameLabels() : excludeFrames)
+			for (j in getFramesArray(sprMovieClip, i))
+				for (k in j)
+					frameCollection.pushFrame(k);
 
 		return frameCollection;
 	}
@@ -53,29 +47,22 @@ class AtlasFrameMaker extends FlxFramesCollection
 	@:noCompletion private static function getFramesArray(t:SpriteMovieClip, animation:String):Array<FlxFrame>
 	{
 		var daFramez:Array<FlxFrame> = [];
-		var rect:Rectangle = new Rectangle(0, 0);
 
-		t.currentLabel = animation;
+		var rect:Rectangle = t.getBounds(t);
 
 		for (i in t.getFrame(animation)...t.numFrames)
 		{
-			t.currentFrame = i;
+			var bitmapShit:BitmapData = new BitmapData(Std.int(rect.width + rect.x), Std.int(rect.height + rect.y), true, 0);
+			bitmapShit.draw(t, true);
 
-			if (t.currentLabel == animation)
-			{
-				rect = t.getBounds(t);
+			var theFrame:FlxFrame = new FlxFrame(FlxGraphic.fromBitmapData(bitmapShit));
+			theFrame.name = animation + i;
+			theFrame.frame = new FlxRect(0, 0, bitmapShit.width, bitmapShit.height);
+			theFrame.sourceSize.set(bitmapShit.width, bitmapShit.height);
+			daFramez.push(theFrame);
 
-				var bitmapShit:BitmapData = new BitmapData(Std.int(rect.width + rect.x), Std.int(rect.height + rect.y), true, 0);
-				bitmapShit.draw(t, true);
-
-				var theFrame:FlxFrame = new FlxFrame(FlxGraphic.fromBitmapData(bitmapShit));
-				theFrame.name = t.currentLabel + t.currentFrame;
-				theFrame.sourceSize.set(bitmapShit.width, bitmapShit.height);
-				theFrame.frame = new FlxRect(0, 0, bitmapShit.width, bitmapShit.height);
-				daFramez.push(theFrame);
-			}
-			else
-				break;
+			bitmapShit.dispose();
+			bitmapShit = null;
 		}
 
 		return daFramez;
