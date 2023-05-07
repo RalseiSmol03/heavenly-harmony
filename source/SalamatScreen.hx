fe57340ffc27f0c22cfc0162fade81e5335a2cd0
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-07 22:02:50 +0300

    Update SalamatScreen.hx

--- source/SalamatScreen.hx
+++ source/SalamatScreen.hx
@@ -16,16 +16,23 @@ class SalamatScreen extends MusicBeatState
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		FlxG.sound.music.stop();
+
 		super.create();
+
 		var thanks:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menus/salamat'));
 		add(thanks);
+
 		FlxG.sound.play(Paths.sound('weekClear'));
 
 		var thankss:FlxText = new FlxText(12, FlxG.height - 44, 0, "Thanks for playing", 12);
 		thankss.scrollFactor.set();
 		thankss.setFormat(Paths.font("GhostKidAOE.otf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(thankss);
+
 		var noproblemo:FlxText = new FlxText(12, FlxG.height - 24, 0, "Go to Freeplay for a Bonus Song!", 12);
 		noproblemo.scrollFactor.set();
 		noproblemo.setFormat(Paths.font("GhostKidAOE.otf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
