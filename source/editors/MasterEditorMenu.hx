a561fdcadd57b8a3d8be8383eb3127d4af49250b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-07 22:10:53 +0300

    Update MasterEditorMenu.hx

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -12,9 +12,6 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.sound.FlxSound;
-#if MODS_ALLOWED
-import sys.FileSystem;
-#end
 
 using StringTools;
 
@@ -28,16 +25,17 @@ class MasterEditorMenu extends MusicBeatState
 		'Character Editor',
 		'Chart Editor'
 	];
-	private var grpTexts:FlxTypedGroup<Alphabet>;
-	private var directories:Array<String> = [null];
 
+	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var curSelected = 0;
-	private var curDirectory = 0;
-	private var directoryTxt:FlxText;
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		FlxG.camera.bgColor = FlxColor.BLACK;
+
 		#if desktop
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("Editors Main Menu", null);
@@ -60,29 +58,10 @@ class MasterEditorMenu extends MusicBeatState
 			leText.snapToPosition();
 		}
 
-		#if MODS_ALLOWED
-		var textBG:FlxSprite = new FlxSprite(0, FlxG.height - 42).makeGraphic(FlxG.width, 42, 0xFF000000);
-		textBG.alpha = 0.6;
-		add(textBG);
-
-		directoryTxt = new FlxText(textBG.x, textBG.y + 4, FlxG.width, '', 32);
-		directoryTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-		directoryTxt.scrollFactor.set();
-		add(directoryTxt);
-
-		for (folder in Paths.getModDirectories())
-		{
-			directories.push(folder);
-		}
-
-		var found:Int = directories.indexOf(Paths.currentModDirectory);
-		if (found > -1)
-			curDirectory = found;
-		changeDirectory();
-		#end
 		changeSelection();
 
 		FlxG.mouse.visible = false;
+
 		super.create();
 	}
 
@@ -92,20 +71,11 @@ class MasterEditorMenu extends MusicBeatState
 		{
 			changeSelection(-1);
 		}
+
 		if (controls.UI_DOWN_P)
 		{
 			changeSelection(1);
 		}
-		#if MODS_ALLOWED
-		if (controls.UI_LEFT_P)
-		{
-			changeDirectory(-1);
-		}
-		if (controls.UI_RIGHT_P)
-		{
-			changeDirectory(1);
-		}
-		#end
 
 		if (controls.BACK)
 		{
@@ -129,6 +99,7 @@ class MasterEditorMenu extends MusicBeatState
 				case 'Chart Editor': // felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
 			}
+
 			FlxG.sound.music.volume = 0;
 			#if PRELOAD_ALL
 			FreeplayState.destroyFreeplayVocals();
@@ -150,6 +121,7 @@ class MasterEditorMenu extends MusicBeatState
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
+
 		super.update(elapsed);
 	}
 
@@ -164,28 +136,4 @@ class MasterEditorMenu extends MusicBeatState
 		if (curSelected >= options.length)
 			curSelected = 0;
 	}
-
-	#if MODS_ALLOWED
-	function changeDirectory(change:Int = 0)
-	{
-		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-
-		curDirectory += change;
-
-		if (curDirectory < 0)
-			curDirectory = directories.length - 1;
-		if (curDirectory >= directories.length)
-			curDirectory = 0;
-
-		WeekData.setDirectoryFromWeek();
-		if (directories[curDirectory] == null || directories[curDirectory].length < 1)
-			directoryTxt.text = '< No Mod Directory Loaded >';
-		else
-		{
-			Paths.currentModDirectory = directories[curDirectory];
-			directoryTxt.text = '< Loaded Mod Directory: ' + Paths.currentModDirectory + ' >';
-		}
-		directoryTxt.text = directoryTxt.text.toUpperCase();
-	}
-	#end
 }
