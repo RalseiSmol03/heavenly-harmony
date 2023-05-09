05892d59d73b26568601276103dcf50aefa40efd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-09 21:29:12 +0000

    ...

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -56,9 +56,9 @@ using StringTools;
 
 class FunkinLua
 {
-	public static var Function_Stop:Dynamic = 0;
-	public static var Function_Continue:Dynamic = 1;
-	public static var Function_StopLua:Dynamic = 2;
+	public static var Function_Stop:Dynamic = "##PSYCHLUA_FUNCTIONSTOP";
+	public static var Function_Continue:Dynamic = "##PSYCHLUA_FUNCTIONCONTINUE";
+	public static var Function_StopLua:Dynamic = "##PSYCHLUA_FUNCTIONSTOPLUA";
 
 	public static var callbacks:Map<String, Function> = [];
 
